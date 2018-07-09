3c3c463fc4cef0df6e084f8893141f654af6fbc2
progof <progof031@gmail.com>
2018-07-09 12:58:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aonecoin
+
+Copyright (c) 2018-2019, The Aonecoin Developers.   
+Portions Copyright (c) 2012-2017, The CryptoNote Developers, The Bytecoin Developers.
+
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018-2019, The Aonecoin Developers.
+ * Portions Copyright (c) 2012-2017, The CryptoNote Developers, The Bytecoin Developers.
+ *
+ * This file is part of Alloy.
+ *
+ * This file is subject to the terms and conditions defined in the
+ * file 'LICENSE', which is part of this source code package.
+ */
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { {  0x83, 0x45, 0x56, 0x44, 0x14, 0x58, 0x91, 0x35, 0x81, 0x33, 0x23, 0x64, 0x46, 0x18, 0x18, 0x92  } };
+}
